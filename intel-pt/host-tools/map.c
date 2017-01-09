160817eecece7edca512e71a9c216bee1ecdd650
Doug Evans <xdje42@gmail.com>
2017-01-09 08:37:50 -0800

    snapshot, more usable indentation
    Change-Id: Iba23d061c3f8d391066de9705721e4993f2e3968

--- intel-pt/host-tools/map.c
+++ intel-pt/host-tools/map.c
@@ -10,27 +10,27 @@ static int pagesize;
 
 static void __attribute__((constructor)) init_ps(void)
 {
-	pagesize = sysconf(_SC_PAGESIZE);
+  pagesize = sysconf(_SC_PAGESIZE);
 }
 
 void *mapfile(char *fn, size_t *size)
 {
-	int fd = open(fn, O_RDWR);
-	if (fd < 0)
-		return NULL;
-	struct stat st;
-	void *map = (void *)-1L;
-	if (fstat(fd, &st) >= 0) {
-		*size = st.st_size;
-		map = mmap(NULL, round_up(st.st_size, pagesize),
-			   PROT_READ|PROT_WRITE,
-			   MAP_PRIVATE, fd, 0);
-	}
-	close(fd);
-	return map != (void *)-1L ? map : NULL;
+  int fd = open(fn, O_RDWR);
+  if (fd < 0)
+    return NULL;
+  struct stat st;
+  void *map = (void *)-1L;
+  if (fstat(fd, &st) >= 0) {
+    *size = st.st_size;
+    map = mmap(NULL, round_up(st.st_size, pagesize),
+               PROT_READ|PROT_WRITE,
+               MAP_PRIVATE, fd, 0);
+  }
+  close(fd);
+  return map != (void *)-1L ? map : NULL;
 }
 
 void unmapfile(void *map, size_t size)
 {
-	munmap(map, round_up(size, pagesize));
+  munmap(map, round_up(size, pagesize));
 }
